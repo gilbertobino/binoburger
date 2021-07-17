74e8eae25166cde43c7fdfa9ffcca5200d58ee05
gilbertobino <gilbertobino.29@gmail.com>
2021-07-17 20:21:20 -0300

    Removed Comments

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::get();
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('admin.post.create');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\{
+    PostController
+};
 use Illuminate\Support\Facades\Route;
+
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 
 Route::get('/', function () {
